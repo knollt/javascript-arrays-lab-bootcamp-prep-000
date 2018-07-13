38397d0bf9adf11174416acdb07dd7aba04cdf88
timothy.knoll@gmail.com <timothy.knoll@gmail.com>
2018-07-13 02:02:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-var array = ['Milo', 'Otis', 'Garfield']
+const array = ['Milo', 'Otis', 'Garfield']
 
 function kittens (array){
   return array
 }
 
 function destructivelyAppendKitten (name) {
-  let array2 = ['Milo', 'Otis', 'Garfield']
-  return array2.push(name)
+  
+  return array.push(name)
   
 }
 
 function destructivelyPrependKitten (name) {
-  let array = ['Milo', 'Otis', 'Garfield']
   return array.unshift(name)
 }
 
 function destructivelyRemoveLastKitten() {
-  let array = ['Milo', 'Otis', 'Garfield']
   // array.splice(-1,1)
   array.pop()
   return array
 }
 
 function destructivelyRemoveFirstKitten() {
-  let array = ['Milo', 'Otis', 'Garfield']
   // array.splice(0,1)
   array.shift()
   return array
